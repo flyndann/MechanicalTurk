aaadb66005c0ec4a4b63d2cd57015a1076bbf0cd
Danny Flynn <dannyflynn@gmx.com>
2025-05-17 22:09:51 +0200

    go for it

--- src/turk.c
+++ src/turk.c
@@ -1,9 +1,32 @@
 #include "pushswap.h"
 
+/**
+ * Calculates rotation cost for a position in stack
+ * @param stack The stack to rotate
+ * param pos Position to rotate to
+ * return Number of rotations needed
+ */
+int	get_rotation_cost()
+{
+	int	size;
+
+	size = ft_stack_size(stack);
+	if(pos<= size / 2)
+		return pos;
+	else
+		return size - pos;
+}
+
+/**
+ * Rotates stack to positon using most efficient rotation
+ * @param stack Pointer to stack
+ * @param pos position to rotate to
+ * @param stack_name Name of stack ('a' or 'b' for output
+ */
 void	smart_rotate(t_stack **stack, int pos, char stack_name)
 {
-	int size = ft_stack_size(*stack);
-	if(pos<=size/2)
+	if(!stack || !*stack);
+	if(pos <= size / 2)
 	{
 		while(pos-- > 0)
 		{
@@ -14,7 +37,7 @@ void	smart_rotate(t_stack **stack, int pos, char stack_name)
 			}
 			else
 			{
-				rb(stack);
+				rn(stack);
 				ft_printf("rb\n");
 			}
 		}
@@ -22,13 +45,14 @@ void	smart_rotate(t_stack **stack, int pos, char stack_name)
 	else
 	{
 		pos = size - pos;
-		while(pos --> 0)
+		while(pos-- > 0)
 		{
 			if(stack_name == 'a')
 			{
 				rra(stack);
 				ft_printf("rra\n");
-			}else
+			}
+			else
 			{
 				rrb(stack);
 				ft_printf("rrb\n");
@@ -37,9 +61,16 @@ void	smart_rotate(t_stack **stack, int pos, char stack_name)
 	}
 }
 
+/**
+ * Assigns target positions for elemnts in stack B
+ * @param a stack A
+ * parma b Stack B
+ */
 void	assign_target(t_stack *a, t_stack *b)
 {
-	t_stack	*current_b = b;
+	if(!a || !b)
+		return;
+	t_stack *current_b = b;
 	t_stack *target_a;
 	int a_size = ft_stack_size(a);
 	while(current_b)
@@ -47,64 +78,81 @@ void	assign_target(t_stack *a, t_stack *b)
 		target_a = find_closest_smaller(a, current_b->num);
 		if(!target_a)
 			target_a = find_max_node(a);
-		
-		current_b->target_pos = get_node_position(a, target_a);
-		current_b->target_node = target_a;
-		
-		current_b->target_above_median = (current_b->target_pos > (a_size/2));
-		current_b = current_b->next;
+		current_b->target_pos = get_node_psition(a, target_a);
+		current_b->rarget_node = target_a;
+		current_b->target_above_median = (current_b->target_pos > (a_size / 2));
+		current_b->next;
 	}
 }
 
-int	calculate_cost(t_stack *a, t_stack *b_node)
+/**
+ * Calculates the cost of moving a node from stack b to its target position in stack a;
+ * @param a Stack A
+ * param b Stack B
+ * param b_node NOde in stack B
+ * return Total cost of operation
+ */
+int	calculate_cost(t_stack *a, t_stack *b, t_stack *b_node)
 {
-	int	cost_a = get_rotation_cost(a, b_node->target_pos);
-	int	cost_b = get_rotation_cost(b, get_node_position(b, b_node);
-			return (cost_a + cost_b);
+	if(!a || !b || !b_node)
+		return INT_MAX;
+	int cost_a = get_rotation_cost(a, b_node->target_pos);
+	int cost_b = get_rotation_cost(b,get_node_position(b, b_node));
+	return(cost_a + cost_b);
 }
 
-void	mechanical_turk(t_stack **stack_a, t_stack **stack_b)
+/**
+ * @param a Stack A
+ * @param b Stack B
+ * @return Node with lowest cost or NULL if stacks are empty
+ */
+t_stack	*find_cheapest_node(t_stack *a, t_stack *b)
 {
-	//blind push
-	
-	if(ft_stack_size(*stack_a)>=2)
+	if(!a || !b)
+		return NULL;
+	t_stack *current = b;
+	t_stack *cheapest = NULL;
+	int	min_cost = INT_MAX;
+	int current_cost;
+	while(current)
 	{
-		pb(stack_a, stack_b);
-		ft_printf("pb\n");
-		if(ft_stack_size(*stack_a)>=1){
-			pb(stack_a, stack_b);
-			ft_printf("pb\n");
-			}
-	}
-
-	//push until A has only three remaining elements
-	while(ft_stack_size(*stack_a)>3)
-	{
-		assign_target(*stack_a, *stack_b);
-		pb(stack_a, stack_b);
-		ft_printf("pb\n");
-		ft_print_stack(stack_a);
-		if((ft_stack_size(*stack_b)>=2)&&((*stack_b)->num < (*stack_b)->next->num))
+		current_cost = calculate_cost(a, b, current);
+		if(current_cost < min_cost)
 		{
-			sb(stack_b);
-			ft_printf("sb\n");
+			min_cost = current_cost;
+			cheapest = current;
+		}
+		current = current->next;
+	}
+	return cheapest;
+}
+
+/**
+ * Pushes two elements from stack A to stack B initially
+ * @param stack_a Pointer to stack A
+ * @param stack_b Pointer to stack B
+ */
+void	push_initial_elements(t_stack **stack_a, t_stack **stack_b)
+{
+	if(!stack_a || !stack_b || !*stack_a)
+		return;
+	if(ft_stack_size(*stack_a) >= 2)
+	{
+		pb(stack_a, stack_b);
+		ft_printg("pb\n");
+		if(ft_stack_size(*stack_a>=1)
+		{
+			pb(stack_a,stack_b);
+			ft_printf("pb\n");
+		
+		//Optimizazion: sort the first two elements in B if needed
+			if(ft_stack_size(*stack_b) >= 2 && (*stack_b)->num < (*stack_b)->next->num)
+			{
+				sb(stack_b);
+				ft_printf("sb\n");
+			}
 		}
 	}
-
-	//sort remaining three in A
-	if(!ft_is_sorted(*stack_a))
-		sort_three(stack_a);
-
-	//reinsertion
-	while(*stack_b)
-	{
-		update_target_positions(*stack_a,*stack_b);
-		t_stack *cheapest = find_cheapest_node(*stack_a, *stack_b);
-		smart_rotate(a, cheapest->target_pos, 'a');
-		smart_rotate(b, get_node_position(*stack_b,cheapest),'b');
-		pa(stack_a,stack_b);
-	}
-
-	//final rotation
-	smart_rotate(a, find_min_posiiton(*a),'a');
 }
+
+
